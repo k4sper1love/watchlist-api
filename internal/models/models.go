45bf1b39981485e36cc8eaddad381f41c2e1450d
k4sper1love <elkin_serega.2004@mail.ru>
2024-08-20 14:16:08 +0500

    feat: add models

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,3 +1,17 @@
 package models
 
-//TODO add Wishlist models
+type User struct {
+	Id        int    `json:"id"`
+	Username  string `json:"username"`
+	CreatedAt string `json:"created_at"`
+}
+
+type Film struct {
+	Id          int     `json:"id"`
+	Title       string  `json:"title"`
+	Year        int     `json:"year"`
+	Genre       string  `json:"genre"`
+	Description string  `json:"description"`
+	Rating      float64 `json:"rating"`
+	PhotoUrl    string  `json:"photo_url"`
+}
