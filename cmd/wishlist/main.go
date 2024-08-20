1a5e011362b9271eb9273c47fa0209e9f2368c31
k4sper1love <elkin_serega.2004@mail.ru>
2024-08-20 14:24:41 +0500

    feat: add init and run server

--- cmd/wishlist/main.go
+++ cmd/wishlist/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "github.com/k4sper1love/wishlist-api/internal/wishlist"
+import (
+	"github.com/k4sper1love/wishlist-api/internal/wishlist"
+)
 
+func init() {
+
+}
 func main() {
 	wishlist.Run()
 }
--- internal/wishlist/wishlist.go
+++ internal/wishlist/wishlist.go
@@ -1,7 +1,30 @@
 package wishlist
 
-import "fmt"
+import (
+	"github.com/k4sper1love/wishlist-api/internal/config"
+	"github.com/k4sper1love/wishlist-api/internal/transport/rest"
+	"log"
+)
 
 func Run() {
-	fmt.Println("Run initialization whole app")
+	log.Println("Run initialization whole app")
+
+	log.Print("Loading .env file")
+	err := config.LoadConfig()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	log.Print("Loading server")
+	err = rest.LoadServer()
+	if err != nil {
+		log.Fatal("Error loading server")
+	}
+
+	log.Println("Run server on", rest.Address)
+	err = rest.Server.ListenAndServe()
+	if err != nil {
+		log.Println(err)
+		return
+	}
 }
