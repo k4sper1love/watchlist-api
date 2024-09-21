ad0546b4ad1e572b332bf937367b73c2e05fe300
k4sper1love <elkin_serega.2004@mail.ru>
2024-09-21 13:24:24 +0500

    feat: add https

--- internal/transport/rest/server.go
+++ internal/transport/rest/server.go
@@ -68,8 +68,8 @@ func Serve() error {
 		sl.Log.Info("starting HTTPS server", slog.String("address", httpsServer.Addr))
 
 		// Creating paths to the SSL certificate and key based on SERVER_HOST
-		certFile := "../../../certs/fullchain.pem"
-		keyFile := "../../../certs/privkey.pem"
+		certFile := "/home/root/watchlist-api/certs/fullchain.pem"
+		keyFile := "/home/root/watchlist-api/certs/privkey.pem"
 
 		// Launching an HTTPS server in goroutine
 		go func() {
