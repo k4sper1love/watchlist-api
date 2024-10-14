be171da6d1ca990374ea7793ed619233c5e1f544
k4sper1love <elkin_serega.2004@mail.ru>
2024-10-14 21:10:58 +0500

    refactor: replace generate token function

--- internal/transport/rest/tokens.go
+++ internal/transport/rest/tokens.go
@@ -1,11 +1,8 @@
 package rest
 
 import (
-	"github.com/golang-jwt/jwt"
-	"github.com/k4sper1love/watchlist-api/internal/config"
 	"github.com/k4sper1love/watchlist-api/internal/database/postgres"
-	"github.com/k4sper1love/watchlist-api/pkg/models"
-	"strconv"
+	"github.com/k4sper1love/watchlist-api/pkg/tokens"
 	"time"
 )
 
@@ -15,32 +12,15 @@ const (
 	refreshTokenExpiration = 48 * time.Hour
 )
 
-// generateToken creates a JWT token for a user with a specified expiration duration.
-func generateToken(ID int, duration time.Duration) (string, error) {
-	expirationTime := time.Now().Add(duration)
-
-	// Create the claims including user ID and expiration time.
-	claims := &models.JWTClaims{
-		Sub: strconv.Itoa(ID),
-		StandardClaims: jwt.StandardClaims{
-			ExpiresAt: expirationTime.Unix(),
-		},
-	}
-
-	// Create and sign the token using the HS256 signing method.
-	token := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), claims)
-	return token.SignedString([]byte(config.JWTSecret))
-}
-
 // generateAccessToken creates a JWT access token for a user with a short expiration time.
-func generateAccessToken(ID int) (string, error) {
-	return generateToken(ID, accessTokenExpiration)
+func generateAccessToken(id int) (string, error) {
+	return tokens.GenerateToken(id, accessTokenExpiration)
 }
 
 // generateAndSaveRefreshToken creates a JWT refresh token for a user with a longer expiration time.
 // It also saves the refresh token in the database.
-func generateAndSaveRefreshToken(ID int) (string, error) {
-	tokenString, err := generateToken(ID, refreshTokenExpiration)
+func generateAndSaveRefreshToken(id int) (string, error) {
+	tokenString, err := tokens.GenerateToken(id, refreshTokenExpiration)
 	if err != nil {
 		return "", err
 	}
@@ -48,7 +28,7 @@ func generateAndSaveRefreshToken(ID int) (string, error) {
 	// Save the refresh token in the database for later use.
 	expirationTime := time.Now().Add(refreshTokenExpiration)
 
-	if err := postgres.SaveRefreshToken(tokenString, ID, expirationTime); err != nil {
+	if err := postgres.SaveRefreshToken(tokenString, id, expirationTime); err != nil {
 		return "", err
 	}
 
